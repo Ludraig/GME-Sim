a2dd3b8fadc7f53b5af5395c1d36c3b5e7136d5b
padraigelkins <pelkins@tfs.ca>
2021-05-24 20:32:39 -0400

    Initial Commit

---
+++ GME Sim/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GME Sim
+//
+//  Created by Padraig Elkins on 2021-05-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
